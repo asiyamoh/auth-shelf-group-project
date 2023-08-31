d2657da210784847482ff9d03a6110364c13534d
fraol <phrolaye@gmail.com>
2023-08-31 14:52:58 -0500

    shelf reducer and saga made

---
+++ src/redux/sagas/shelf.saga.js
@@ -0,0 +1,26 @@
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+
+function* fetchShelf() {
+    try {
+        
+        const shelf = yield axios.get('/api/shelf');
+        console.log('get all:', shelf.data);
+        
+        yield put({ 
+            type: 'SET_SHELF', 
+            payload: shelf.data
+        });
+
+    } catch {
+        console.log('get all error');
+    }
+        
+}
+
+function* getSaga() {  
+    yield takeEvery('FETCH_SHELF', fetchShelf);
+}
+
+export default getSaga;
